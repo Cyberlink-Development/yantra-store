f3168d96ec4414571d27177fc841d2b3cb8dd376
anilcyberlink <anilcyberlink@gmail.com>
2025-12-31 13:06:00 +0545

    User dashboard

--- resources/views/backend/pages/product/index.blade.php
+++ resources/views/backend/pages/product/index.blade.php
@@ -1,18 +1,16 @@
 @extends('backend.layouts.master')
 @section('breadcrum')
-    @include('backend.layouts.breadcrum', ['title' => 'Product List','actionLabel'=>'Create',
-      'actionLink'=>route('product.create'),'backLabel'=>'','backLink'=>''])
+    @include('backend.layouts.breadcrum', ['title' => 'Product List', 'actionLabel' => 'Create', 'actionLink' => route('product.create'), 'backLabel' => '', 'backLink' => ''])
 @endsection
 @section('content')
+<div class="card">
+    <div class="card-header">
+        <h3 style="text-align:center">All Products</h3>
+    </div>
 
-    <div class="card">
-        <div class="card-header">
-            <h3 style="text-align:center">All Products</h3>
-        </div>
-        <!-- /.card-header -->
-        <div class="card-body">
-            <table id="package_table" class="table table-bordered datatable">
-                <thead>
+    <div class="card-body">
+        <table id="package_table" class="table table-bordered datatable">
+            <thead>
                 <tr class="table table-primary">
                     <th>ID</th>
                     <th>Title</th>
@@ -27,27 +25,28 @@
                     <!-- <th>On Sale</th> -->
                     <th>Action</th>
                 </tr>
-                </thead>
-                <tbody>
-                @foreach($products as $key=>$product)
+            </thead>
+            <tbody>
+                @foreach($products as $key => $product)
                     <tr>
-                        <td>{{ $key+=1 }}</td>
+                        <td>{{ $key += 1 }}</td>
                         <td>{{ $product->product_name }}</td>
                         <td>{{ $product->sku }}</td>
                         <td>
                             <input type="checkbox" name="is_featured" {{ $product->is_featured == 1 ? 'checked' : '' }} />
                         </td>
                         <td>
-                            <input type="checkbox" class="toggle-status" data-id="{{ $product->id }}" name="status" onclick="updateStatus(this, {{$product->id}},'{{getModelPathFromData($product)}}')" {{ $product->status == '1' ? 'checked' : '' }}>
+                            <input type="checkbox" class="toggle-status" data-id="{{ $product->id }}" name="status"
+                                onclick="updateStatus(this, {{$product->id}},'{{getModelPathFromData($product)}}')" {{ $product->status == '1' ? 'checked' : '' }}>
                             <!-- @if($product->status==1)
-                                <button class="btn btn-success btn btn-sm" name="active">
-                                    <i class="fa fa-check"></i>
-                                </button>
-                            @else
-                                <button class="btn btn-danger btn btn-sm" name="inactive">
-                                    <i class="fa fa-times"></i>
-                                </button>
-                            @endif -->
+                                    <button class="btn btn-success btn btn-sm" name="active">
+                                        <i class="fa fa-check"></i>
+                                    </button>
+                                @else
+                                    <button class="btn btn-danger btn btn-sm" name="inactive">
+                                        <i class="fa fa-times"></i>
+                                    </button>
+                                @endif -->
                         </td>
                         <td>
                             @foreach($product->categories as $value)
@@ -55,73 +54,73 @@
                             @endforeach
                         </td>
                         <td>
-                            <img src="{{$img->get_main_image($product->id) ? asset('/images/products/'.$img->get_main_image($product->id)) : asset('images/default-thumbnail.jpeg')}}" width="100px" alt="product">
+                            <img src="{{$img->get_main_image($product->id) ? asset('/images/products/' . $img->get_main_image($product->id)) : asset('images/default-thumbnail.jpeg')}}"
+                                width="100px" alt="product">
                         </td>
                         <td>
                             {{ $product->stock }}
                             {{--@if($product->size_variation == 0)
-                                @foreach($product->colorstocks as $stock)
-                                    <li>
-                                        {{ $stock->title }}&nbsp;:&nbsp;{{ $stock->pivot->stock }}
+                            @foreach($product->colorstocks as $stock)
+                            <li>
+                                {{ $stock->title }}&nbsp;:&nbsp;{{ $stock->pivot->stock }}
 
-                                    </li>
-                                @endforeach
+                            </li>
+                            @endforeach
                             @else
-                                @if($product->size_variation == 1)
-                                    <ul>
-                                        @foreach($product->stocks as $stock)
-                                            <li>
-                                                {{ $stock->size->title }}&nbsp;:&nbsp;{{ $stock->stock }}
-                                            </li>
-                                        @endforeach
-                                    </ul>
-                               @endif
+                            @if($product->size_variation == 1)
+                            <ul>
+                                @foreach($product->stocks as $stock)
+                                <li>
+                                    {{ $stock->size->title }}&nbsp;:&nbsp;{{ $stock->stock }}
+                                </li>
+                                @endforeach
+                            </ul>
+                            @endif
                             @endif --}}
                         </td>
                         <td>
                             <a href="{{route('all-review', $product->id)}}">{{$product->reviews->count()}}</a>
                         </td>
                         <!-- <td>
-                            @if($product->is_special==1)
-                                <button class="btn btn-success btn btn-sm" name="active"><i
-                                        class="fa fa-check"></i>
-                                </button>
-                            @else
-                                <button class="btn btn-danger btn btn-sm" name="inactive"><i
-                                        class="fa fa-times"></i>
-                                </button>
-                            @endif
-                        </td> -->
+                                @if($product->is_special==1)
+                                    <button class="btn btn-success btn btn-sm" name="active"><i
+                                            class="fa fa-check"></i>
+                                    </button>
+                                @else
+                                    <button class="btn btn-danger btn btn-sm" name="inactive"><i
+                                            class="fa fa-times"></i>
+                                    </button>
+                                @endif
+                            </td> -->
                         <!-- <td>
-                            @if($product->on_sale==1)
-                                <button class="btn btn-success btn btn-sm" name="active"><i
-                                        class="fa fa-check"></i>
-                                </button>
-                            @else
-                                <button class="btn btn-danger btn btn-sm" name="inactive"><i
-                                        class="fa fa-times"></i>
-                                </button>
-                            @endif
-                        </td> -->
+                                @if($product->on_sale==1)
+                                    <button class="btn btn-success btn btn-sm" name="active"><i
+                                            class="fa fa-check"></i>
+                                    </button>
+                                @else
+                                    <button class="btn btn-danger btn btn-sm" name="inactive"><i
+                                            class="fa fa-times"></i>
+                                    </button>
+                                @endif
+                            </td> -->
                         <td>
                             <a class="btn btn-sm btn-outline-primary confirm"
-                               href="{{route('product.edit',$product->id)}}"><i class="fa fa fa-edit"></i>
+                                href="{{route('product.edit', $product->id)}}"><i class="fa fa fa-edit"></i>
                             </a>
                             <!-- <a class="btn btn-sm btn-outline-primary confirm"
-                               href="{{route('show-product',$product->slug)}}"
-                            ><i class="fa fa fa-eye"></i></a> -->
+                                   href="{{route('show-product',$product->slug)}}"
+                                ><i class="fa fa fa-eye"></i></a> -->
                             <a class="btn btn-sm btn-outline-primary confirm"
-                               href="{{route('delete-product',$product->id)}}"
-                               onclick="return confirm('Confirm Delete?')"
-                            ><i class="fa fa fa-trash"></i></a>
+                                href="{{route('delete-product', $product->id)}}"
+                                onclick="return confirm('Confirm Delete?')"><i class="fa fa fa-trash"></i></a>
                         </td>
                     </tr>
-            @endforeach
-                </tbody>
-            </table>
+                @endforeach
+            </tbody>
+        </table>
 
-        </div>
     </div>
+</div>
 
 @stop
 @push('scripts')
