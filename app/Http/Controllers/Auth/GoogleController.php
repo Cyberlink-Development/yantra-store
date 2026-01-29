3a6a9aa267710dee326db98db9c74400eea670a9
anilcyberlink <anilcyberlink@gmail.com>
2026-01-29 11:54:09 +0545

    idk

--- app/Http/Controllers/Auth/GoogleController.php
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -18,7 +18,7 @@ class GoogleController extends Controller
     public function handleGoogleCallback()
     {
         try {
-            $googleUser = Socialite::driver('google')->stateless()->user();
+            $googleUser = Socialite::driver('google')->user();
 
             $user = User::updateOrCreate(
                 ['email' => $googleUser->getEmail()],
